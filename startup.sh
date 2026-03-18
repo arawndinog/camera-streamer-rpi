c202bbe1ae45173b8701df74feb2736f4717ffce
arawndinog <arawndinog@gmail.com>
2026-03-18 23:49:20 +0800

    add git pull to pull newest ver for when overlayFS is enabled

--- startup.sh
+++ startup.sh
@@ -3,5 +3,9 @@
 source $HOME/miniconda3/bin/activate
 conda activate cam
 cd $HOME/Projects/camera-streamer-rpi
-python -u cam.py
 
+# git pull fetches latest changes into the tmpfs layer when overlayFS is enabled
+# if network is unavailable, falls back to the version baked into the base image
+git pull || echo "git pull failed, running existing version"
+
+python -u cam.py
